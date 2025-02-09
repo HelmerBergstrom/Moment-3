cd140cd0d21c8a664b919605791c4a2f2dc922d0
HelmerBergstrom <hebe2400@student.miun.se>
2025-02-09 16:10:39 +0100

    Skapar första sass-filen. Stilar menyn och tar tillbaka javascript-funktion för hamburgar-meny från tidigare moment.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburgare = document.querySelector(".hamburger");
+    const mobilMeny = document.querySelector(".mobile-menu");
+
+    hamburgare.addEventListener("click", () => {
+        mobilMeny.classList.toggle("open");
+    })
+})
